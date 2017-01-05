81a547136c1851813be346014f2d8cf979e8624b
welly <wellysalim@gmail.com>
2017-01-05 19:08:17 +0800

    Adding test for Place Action

--- lib/action/place.rb
+++ lib/action/place.rb
@@ -2,13 +2,14 @@ require_relative 'action.rb'
 
 module Game
   class Place < Game::Action
+
+    private
     def execute_command(x, y, direction)
       if command =~ command_pattern
         return $1.to_i, $2.to_i, $3
       end
     end
 
-    private
     def command_pattern
        /^PLACE\s+(\d+),(\d+),(WEST|EAST|NORTH|SOUTH)$/
     end
